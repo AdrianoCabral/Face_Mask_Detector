74e649f9c1d2e2ca50b2e869e93671b484f4eae9
AdrianoCabral <afcf@cin.ufpe.br>
2021-08-19 12:47:02 -0300

    train_mask_detector reading image dataset

---
+++ train_mask_detector.py
@@ -0,0 +1,104 @@
+# import the necessary packages
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.layers import AveragePooling2D
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from imutils import paths
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import os
+
+# construct the argument parser and parse the arguments
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-d", "--dataset", required=True,
+#   help="path to input dataset")
+# ap.add_argument("-p", "--plot", type=str, default="plot.png",
+#   help="path to output loss/accuracy plot")
+# ap.add_argument("-m", "--model", type=str,
+#   default="mask_detector.model",
+#   help="path to output face mask detector model")
+# args = vars(ap.parse_args())
+
+# initialize the initial learning rate, number of epochs to train for,
+# and batch size
+INIT_LR = 1e-4
+EPOCHS = 20
+BS = 32
+
+print("[INFO] loading images...")
+imagePaths = list(paths.list_images('./dataset'))
+data = []
+labels = []
+# loop over the image paths
+for imagePath in imagePaths:
+    # extract the class label from the filename
+    label = imagePath.split(os.path.sep)[-2]
+    print(label)
+    # load the input image (224x224) and preprocess it
+    image = load_img(imagePath, target_size=(224, 224))
+    image = img_to_array(image)
+    image = preprocess_input(image)
+    # update the data and labels lists, respectively
+    data.append(image)
+    labels.append(label)
+# convert the data and labels to NumPy arrays
+data = np.array(data, dtype="float32")
+labels = np.array(labels)
+
+lb = LabelBinarizer()
+labels = lb.fit_transform(labels)
+labels = to_categorical(labels)
+
+# model =Sequential([
+#     Conv2D(100, (3,3), activation='relu', input_shape=(150, 150, 3)),
+#     MaxPooling2D(2,2),
+    
+#     Conv2D(100, (3,3), activation='relu'),
+#     MaxPooling2D(2,2),
+    
+#     Flatten(),
+#     Dropout(0.5),
+#     Dense(50, activation='relu'),
+#     Dense(2, activation='softmax')
+# ])
+# model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+
+# TRAINING_DIR = "./train"
+# train_datagen = ImageDataGenerator(rescale=1.0/255,
+#                                    rotation_range=40,
+#                                    width_shift_range=0.2,
+#                                    height_shift_range=0.2,
+#                                    shear_range=0.2,
+#                                    zoom_range=0.2,
+#                                    horizontal_flip=True,
+#                                    fill_mode='nearest')
+
+# train_generator = train_datagen.flow_from_directory(TRAINING_DIR, 
+#                                                     batch_size=10, 
+#                                                     target_size=(150, 150))
+# VALIDATION_DIR = "./test"
+# validation_datagen = ImageDataGenerator(rescale=1.0/255)
+
+# validation_generator = validation_datagen.flow_from_directory(VALIDATION_DIR, 
+#                                                          batch_size=10, 
+#                                                          target_size=(150, 150))
+# checkpoint = ModelCheckpoint('model2-{epoch:03d}.model',monitor='val_loss',verbose=0,save_best_only=True,mode='auto')
+
+
+# history = model.fit_generator(train_generator,
+#                               epochs=10,
+#                               validation_data=validation_generator,
+#                               callbacks=[checkpoint])
