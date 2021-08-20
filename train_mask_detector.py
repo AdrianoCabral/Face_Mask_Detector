4540597577d7e1b6e5101855644021bdc865c32c
AdrianoCabral <afcf@cin.ufpe.br>
2021-08-20 17:38:56 -0300

    training pipeling v1

--- train_mask_detector.py
+++ train_mask_detector.py
@@ -1,16 +1,11 @@
 # import the necessary packages
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications import MobileNetV2
-from tensorflow.keras.layers import AveragePooling2D
-from tensorflow.keras.layers import Dropout
-from tensorflow.keras.layers import Flatten
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import Input
-from tensorflow.keras.models import Model
+from tensorflow.keras.layers import AveragePooling2D,Dropout,Flatten, Dense, Input,Conv2D,MaxPooling2D
+from tensorflow.keras.models import Model, Sequential
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
-from tensorflow.keras.preprocessing.image import img_to_array
-from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array,load_img
 from tensorflow.keras.utils import to_categorical
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
@@ -46,7 +41,6 @@ labels = []
 for imagePath in imagePaths:
     # extract the class label from the filename
     label = imagePath.split(os.path.sep)[-2]
-    print(label)
     # load the input image (224x224) and preprocess it
     image = load_img(imagePath, target_size=(224, 224))
     image = img_to_array(image)
@@ -62,19 +56,63 @@ lb = LabelBinarizer()
 labels = lb.fit_transform(labels)
 labels = to_categorical(labels)
 
-# model =Sequential([
-#     Conv2D(100, (3,3), activation='relu', input_shape=(150, 150, 3)),
-#     MaxPooling2D(2,2),
+(trainX, testX, trainY, testY) = train_test_split(data, labels,
+	test_size=0.20, stratify=labels, random_state=42)
+
+aug = ImageDataGenerator(
+	rotation_range=20,
+	zoom_range=0.15,
+	width_shift_range=0.2,
+	height_shift_range=0.2,
+	shear_range=0.15,
+	horizontal_flip=True,
+	fill_mode="nearest")
+
+model =Sequential([
+    Conv2D(100, (3,3), activation='relu', input_shape=(224, 224, 3)),
+    MaxPooling2D(2,2),
     
-#     Conv2D(100, (3,3), activation='relu'),
-#     MaxPooling2D(2,2),
+    Conv2D(100, (3,3), activation='relu'),
+    MaxPooling2D(2,2),
     
-#     Flatten(),
-#     Dropout(0.5),
-#     Dense(50, activation='relu'),
-#     Dense(2, activation='softmax')
-# ])
-# model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+    Flatten(),
+    Dropout(0.5),
+    Dense(128, activation='relu'),
+
+    Dense(2, activation='softmax')
+])
+optim = Adam(Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS))
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+
+H = model.fit(
+	aug.flow(trainX, trainY, batch_size=BS),
+	steps_per_epoch=len(trainX) // BS,
+	validation_data=(testX, testY),
+	validation_steps=len(testX) // BS,
+	epochs=EPOCHS)
+
+predIdxs = model.predict(testX, batch_size=BS)
+predIdxs = np.argmax(predIdxs, axis=1)
+
+print(classification_report(testY.argmax(axis=1), predIdxs,
+	target_names=lb.classes_))
+
+print("[INFO] saving mask detector model...")
+model.save('./mask_detector.model', save_format="h5")
+
+# plot the training loss and accuracy
+# N = EPOCHS
+# plt.style.use("ggplot")
+# plt.figure()
+# plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+# plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+# plt.plot(np.arange(0, N), H.history["accuracy"], label="train_acc")
+# plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_acc")
+# plt.title("Training Loss and Accuracy")
+# plt.xlabel("Epoch #")
+# plt.ylabel("Loss/Accuracy")
+# plt.legend(loc="lower left")
+# plt.savefig('./plot.png')
 
 # TRAINING_DIR = "./train"
 # train_datagen = ImageDataGenerator(rescale=1.0/255,
