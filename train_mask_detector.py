dafeffb212def5f8fc0d1479a5de734a363595ad
AdrianoCabral <afcf@cin.ufpe.br>
2021-08-30 16:38:38 -0300

    training pipeline v2

--- train_mask_detector.py
+++ train_mask_detector.py
@@ -57,7 +57,11 @@ labels = lb.fit_transform(labels)
 labels = to_categorical(labels)
 
 (trainX, testX, trainY, testY) = train_test_split(data, labels,
-	test_size=0.20, stratify=labels, random_state=42)
+	test_size=0.30, stratify=labels, random_state=39)
+print(len(testX))
+print(len(testY))
+(testX, valX, testY, valY) = train_test_split(testX, testY,
+	test_size=0.50, stratify=testY, random_state=42)
 
 aug = ImageDataGenerator(
 	rotation_range=20,
@@ -81,14 +85,14 @@ model =Sequential([
 
     Dense(2, activation='softmax')
 ])
-optim = Adam(Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS))
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+optim = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+model.compile(optimizer=optim, loss='binary_crossentropy', metrics=['acc'])
 
 H = model.fit(
 	aug.flow(trainX, trainY, batch_size=BS),
 	steps_per_epoch=len(trainX) // BS,
-	validation_data=(testX, testY),
-	validation_steps=len(testX) // BS,
+	validation_data=(valX, valY),
+	validation_steps=len(valX) // BS,
 	epochs=EPOCHS)
 
 predIdxs = model.predict(testX, batch_size=BS)
@@ -100,43 +104,37 @@ print(classification_report(testY.argmax(axis=1), predIdxs,
 print("[INFO] saving mask detector model...")
 model.save('./mask_detector.model', save_format="h5")
 
-# plot the training loss and accuracy
-# N = EPOCHS
-# plt.style.use("ggplot")
-# plt.figure()
-# plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
-# plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
-# plt.plot(np.arange(0, N), H.history["accuracy"], label="train_acc")
-# plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_acc")
-# plt.title("Training Loss and Accuracy")
-# plt.xlabel("Epoch #")
-# plt.ylabel("Loss/Accuracy")
-# plt.legend(loc="lower left")
-# plt.savefig('./plot.png')
+##fine tuning pipeline
+baseModel = MobileNetV2(weights="imagenet", include_top=False,
+    input_tensor=Input(shape=(224, 224, 3)))
 
-# TRAINING_DIR = "./train"
-# train_datagen = ImageDataGenerator(rescale=1.0/255,
-#                                    rotation_range=40,
-#                                    width_shift_range=0.2,
-#                                    height_shift_range=0.2,
-#                                    shear_range=0.2,
-#                                    zoom_range=0.2,
-#                                    horizontal_flip=True,
-#                                    fill_mode='nearest')
+headModel = baseModel.output
+headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
+headModel = Flatten(name="flatten")(headModel)
+headModel = Dense(128, activation="relu")(headModel)
+headModel = Dropout(0.5)(headModel)
+headModel = Dense(2, activation="softmax")(headModel)
 
-# train_generator = train_datagen.flow_from_directory(TRAINING_DIR, 
-#                                                     batch_size=10, 
-#                                                     target_size=(150, 150))
-# VALIDATION_DIR = "./test"
-# validation_datagen = ImageDataGenerator(rescale=1.0/255)
+fine_tuned_model = Model(inputs=baseModel.input, outputs=headModel)
 
-# validation_generator = validation_datagen.flow_from_directory(VALIDATION_DIR, 
-#                                                          batch_size=10, 
-#                                                          target_size=(150, 150))
-# checkpoint = ModelCheckpoint('model2-{epoch:03d}.model',monitor='val_loss',verbose=0,save_best_only=True,mode='auto')
+#freezing hidden layers
+for layer in baseModel.layers:
+    layer.trainable= False
 
+fine_tuned_model.compile(optimizer=optim, loss='binary_crossentropy', metrics=['acc'])
 
-# history = model.fit_generator(train_generator,
-#                               epochs=10,
-#                               validation_data=validation_generator,
-#                               callbacks=[checkpoint])
+fine_tuned_H = fine_tuned_model.fit(
+	aug.flow(trainX, trainY, batch_size=BS),
+	steps_per_epoch=len(trainX) // BS,
+	validation_data=(valX, valY),
+	validation_steps=len(valX) // BS,
+	epochs=EPOCHS)
+
+predIdxs = fine_tuned_model.predict(testX, batch_size=BS)
+predIdxs = np.argmax(predIdxs, axis=1)
+
+print(classification_report(testY.argmax(axis=1), predIdxs,
+	target_names=lb.classes_))
+
+print("[INFO] saving fine tuned mask detector model...")
+fine_tuned_model.save('./fine_tuned_mask_detector.model', save_format="h5")
